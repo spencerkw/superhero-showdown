7066b38201903ca6462012edabb26ac6c2e11226
Andre <ajsmoothdre@gmail.com>
2019-06-14 14:12:27 -0400

    Created Character Select Screen

--- src/app/characterselection/characterselection.component.ts
+++ src/app/characterselection/characterselection.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'characterselection',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./characterselection.component.css']
 })
 export class CharacterSelectionComponent implements OnInit {
+  heroes: any;
+  selectedHeroes: any[] = [];
 
-  constructor() { }
+  constructor(private apiService: ApiService) { }
 
   ngOnInit() {
+    this.apiService.getHeroes().subscribe(response => {
+      this.heroes=response;
+    });
+  }
+
+  selectHero(index: number) {
+    this.selectedHeroes.push(this.heroes[index]);
+    this.heroes.splice(index, 1);
   }
 
 }
