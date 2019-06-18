282f920aac2fb067baf5b76441a0632f32ddc71b
nick <njperry507@gmail.com>
2019-06-18 11:04:31 -0400

    card now takes hero and displays data

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Hero } from '../hero';
 
 @Component({
   selector: 'card',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent implements OnInit {
+
+  @Input() hero: Hero;
 
   constructor() { }
 
