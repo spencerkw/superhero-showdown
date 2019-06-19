ea36e34bb4a65178e7d42a491cc908e61d177103
Andre <ajsmoothdre@gmail.com>
2019-06-19 11:39:21 -0400

    Created win/lose component

---
+++ src/app/win-lose/win-lose.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { WinLoseComponent } from './win-lose.component';
+
+describe('WinLoseComponent', () => {
+  let component: WinLoseComponent;
+  let fixture: ComponentFixture<WinLoseComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ WinLoseComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(WinLoseComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
