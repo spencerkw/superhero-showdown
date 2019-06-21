9951b5ede22bc1a2df3b1a11ec2bc28648a582ca
nick <njperry507@gmail.com>
2019-06-21 15:25:20 -0400

    removed uneeded test code

--- src/app/battle/battle.component.ts
+++ src/app/battle/battle.component.ts
@@ -73,16 +73,6 @@ import { AnimationDurations } from '../animation-durations';
   ]
 })
 
-// transition('* => move',
-//   animate('2000ms', keyframes([
-//     style({transform: 'translateX(0)    rotateY(0)',        offset: 0}),
-//     style({transform: 'translateX(50%)  rotateY(90deg)',    offset: 0.33}),
-//     style({transform: 'translateY(-75%) rotateY(180deg)',   offset: 0.66}),
-//     style({transform: 'translateX(-100%)',                  offset: 1.0})
-//   ])
-// ))
-
-
 export class BattleComponent implements OnInit {
   playerHeroes: Hero[];
   computerHeroes: Hero[];
